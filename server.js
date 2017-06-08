d39146bbc818bf3a9e67bdbeadb56802309b0bb1
Akbar Hossain Robin <akbarhossain15@gmail.com>
2017-06-08 06:14:17 +0600

    public route added

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
+/*app.get('/', function (req, res) {
 	res.send("Hello World!, I am learning mean stack from the beginning.");
-});
+});*/
+
+app.use(express.static(__dirname + '/public'));
 
 app.listen(3000);
 console.log('Server is running from Server.js');
